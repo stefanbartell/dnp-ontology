8bec5afbaa1b471c8e91eab0054453f032fcd2e3
Michael Chary <mac389@gmail.com>
2022-08-18 11:25:46 -0400

    Update evaluate_challenge_questions.py

--- src/evaluate_challenge_questions.py
+++ src/evaluate_challenge_questions.py
@@ -1,11 +1,25 @@
 import os, json 
 
-from owlready2 import *
-from pyld import jsonld
 import pandas as pd
 
+from owlready2 import *
+from pyld import jsonld
 
-get_names = lambda ontoQuery: ' '.join(item.name for item in ontoQuery)
+#It would be interesting to write an alternate solution that uses recursion
+def get_names(ontoQuery):
+	if all([type(item)==type(Thing) for item in ontoQuery]):
+		return [item.name for item in ontoQuery]
+	else: 
+		ans = []
+		for item in ontoQuery:
+			if type(item) == type(Thing):
+				ans += [item.name]
+			elif type(item) == class_construct.Restriction:
+				print(item.equivalent_to, type(item))
+				ans += ["Bort"]
+			else:
+				ans += [f'Type Unknown:{type(item)}']
+		return ans
 
 data = os.path.join('..','data')
 filename = {"ontology":os.path.join(data,'external','ontologies',"structural_derivatives_benzene.owl"),
@@ -13,41 +27,42 @@ filename = {"ontology":os.path.join(data,'external','ontologies',"structural_der
 
 onto = get_ontology(f'file://{filename["ontology"]}').load()
 
+class aThing(Thing):
+	pass
+
 jld = json.load(open(filename['challenge_questions'],'r'))
 
 cqs = jsonld.compact(jld["doc"],jld["context"])
 
 for cq in jld["doc"]:
-	# print(eval(cq["expression"]))
 	performance = get_names(eval(cq["expression"]))
-	# print(performance)
-	print(f'Generated answer: {set(performance.split())}. Expected: {set(cq["true_answer"])}')
-	print(set(cq["true_answer"]) == set(performance.split()))
+	print(f'Generated answer: {set(performance)}. Expected: {set(cq["true_answer"])}')
+	print(set(cq["true_answer"]) == set(performance))
 
 	#How to handle expressions?
 	#How to store performance results?
 
-columns1 = ["Challenge Question", "Expected Answer", "Generated Answer", "Correct?"]
+# columns1 = ["Challenge Question", "Expected Answer", "Generated Answer", "Correct?"]
 
-data = []
-for i in list(range(len(jld["doc"]))):
-	data += [list(range(len(columns1)))]
+# data = []
+# for i in list(range(len(jld["doc"]))):
+# 	data += [list(range(len(columns1)))]
 
-performance_results = pd.DataFrame(data, columns = columns1)
+# performance_results = pd.DataFrame(data, columns = columns1)
 
-for i in range(len(jld["doc"])):
-	cq = jld["doc"][i]
-	cq_label = cq['label']
-	performance_results.loc[performance_results.index[i], 'Challenge Question'] = cq_label
-	performance = get_names(eval(cq["expression"]))
-	generated_answer = set(performance.split())
-	performance_results.loc[performance_results.index[i], 'Generated Answer'] = str(generated_answer)
-	expected_answer = set(cq["true_answer"])
-	performance_results.loc[performance_results.index[i], 'Expected Answer'] = str(expected_answer)
-	if generated_answer == expected_answer:
-		performance_results.loc[performance_results.index[i], 'Correct?'] = 1
-	else:
-		performance_results.loc[performance_results.index[i], 'Correct?'] = 0
+# for i in range(len(jld["doc"])):
+# 	cq = jld["doc"][i]
+# 	cq_label = cq['label']
+# 	performance_results.loc[performance_results.index[i], 'Challenge Question'] = cq_label
+# 	performance = get_names(eval(cq["expression"]))
+# 	generated_answer = set(performance.split())
+# 	performance_results.loc[performance_results.index[i], 'Generated Answer'] = str(generated_answer)
+# 	expected_answer = set(cq["true_answer"])
+# 	performance_results.loc[performance_results.index[i], 'Expected Answer'] = str(expected_answer)
+# 	if generated_answer == expected_answer:
+# 		performance_results.loc[performance_results.index[i], 'Correct?'] = 1
+# 	else:
+# 		performance_results.loc[performance_results.index[i], 'Correct?'] = 0
 
 
-performance_results.to_csv('performance_results.csv')
+# performance_results.to_csv('performance_results.csv')
